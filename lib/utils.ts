42f5cb05a30bb46b588ba8596609230946a9ba0e
IykeMich <iykemich361@gmail.com>
2025-02-19 20:58:53 +0100

    Cart SideBar Created

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,6 +18,7 @@ text
   .replace(/[^\w\s-]+/g, '')
   .replace(/\s+/g, '-')
   .replace(/^-+|-+$/g, '')
+  .replace(/-+/g, '-')
 
 const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
   currency: 'USD',
